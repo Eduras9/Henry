f6752e065dff79652ec834b6f09871304fd8a714
Gomez1028 <fabioandres013@gmail.com>
2024-08-17 20:31:47 -0500

    C6

--- C6 Bucles/ejercicios/06.js
+++ C6 Bucles/ejercicios/06.js
@@ -2,6 +2,14 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+
+  if (Math.abs(num) > 100 && Math.abs(num) < 1000 ) {
+    return true
+  } else {
+    return false
+  }
 }
 
 module.exports = tieneTresDigitos;
+
+console.log(tieneTresDigitos(-12))
--- C6 Bucles/ejercicios/23.js
+++ C6 Bucles/ejercicios/23.js
@@ -6,6 +6,21 @@ function esNumeroPrimo(numero) {
   // IMPORTANTE: Recuerda que un número primo es aquel que
   // solo es divisible por sí mismo y por 1.
   // Tu código:
+  if(numero <= 1){
+    return false
+  }
+  // Verificamos la divisibilidad desde 2 hasta la raíz cuadrada del número
+  for(var i = 2; i<=Math.sqrt(numero); i++){
+    if(numero % i === 0){//verifamos que no hayan divisores
+      return false
+    }
+  }
+  return true
 }
 
 module.exports = esNumeroPrimo;
+
+console.log(esNumeroPrimo(2))
+
+var num = (4 % 2)
+console.log(num)
